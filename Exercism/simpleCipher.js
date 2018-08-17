3ab2a3a295cfcea18f2808aaf3332e9ed79756c8
thegreatsantini <lss13@my.fsu.edu>
2018-08-17 15:57:58 -0700

    code wars algo

--- Exercism/simpleCipher.js
+++ Exercism/simpleCipher.js
@@ -7,15 +7,34 @@ Given the key "ddddddddddddddddd", encoding our string "iamapandabear" would ret
 const encode = (str, key) => {
   let encodedStr = '';
   const alpha = 'abcdefghijklmnopqrstuvwxyz'
-
+  let keyIndex = 0;
   for (let i = 0; i < str.length; i++) {
-    const charCode = str.charCodeAt(i) - 97;
-    const toCharFromCode = String.fromCharCode(0)
-    console.log(toCharFromCode)
+    if ( keyIndex = key.length ) {
+      keyIndex = 0;
+    }
+    const newCharIndex = (str[i].charCodeAt(0) - 97) + (key[keyIndex].charCodeAt(0) - 97);
+    encodedStr += alpha[newCharIndex]
+    keyIndex++
   }
-  // return encodedStr
+  return encodedStr
 };
 
-// console.log(encode('iamapandabear', 'aaaaaaaaaaaaaaaaaa'));
+const decode = (str, key) => {
+  let decodedStr = '';
+  const alpha = 'abcdefghijklmnopqrstuvwxyz'
+  let keyIndex = 0;
+  
+  for (let i = 0; i < str.length; i++) {
+    if ( keyIndex = key.length ) {
+      keyIndex = 0;
+    }
+    const newCharIndex = (str[i].charCodeAt(0) - 97) - (key[keyIndex].charCodeAt(0) - 97);
+    decodedStr += alpha[newCharIndex]
+    keyIndex++
+  }
+  return decodedStr
+};
+
+console.log(decode('ldpdsdqgdehdu', 'a'));
 // console.log('H'.charCodeAt(0) - 97)
-console.log('i'.charCodeAt(0)-97)
+// console.log('i'.charCodeAt(0)-97)
