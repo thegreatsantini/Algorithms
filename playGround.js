865fbf46451a58e9bef4eb3390aa99b318913d5f
thegreatsantini <lss13@my.fsu.edu>
2018-08-26 14:01:15 -0400

    soleved an algo

---
+++ playGround.js
@@ -0,0 +1,18 @@
+// const iterable = ['Turple', 'other' , 'Rokko']
+
+// for (const [index, item] of iterable.entries() ) {
+//     console.log(`Index: ${index}, Item: ${item}`);
+// };
+
+const user = {
+    name: 'Lucas',
+    last: 'Lst name',
+    links : {
+        faceBook: 'FaceBook Link',
+        twitter: 'Twitter Link'
+    }
+};
+
+const { name, last } = user
+
+console.log(user)
