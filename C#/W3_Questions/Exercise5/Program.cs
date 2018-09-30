30cdf7b4f5c26146ca2563d400ca815824ca2260
thegreatsantini <lss13@my.fsu.edu>
2018-09-29 18:44:12 -0700

    completed an algo

--- C#/W3_Questions/Exercise5/Program.cs
+++ C#/W3_Questions/Exercise5/Program.cs
@@ -4,9 +4,20 @@ namespace Exercise5
 {
     class Program
     {
+        public static void Swap(ref int num1, ref int num2) 
+        {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int A = 5;
+            int B = 6;
+            Console.WriteLine("Before Swap a={0} , b={1}", A, B);
+            Swap(ref A, ref B);
+            Console.WriteLine("After swap a={0}, b={1}", A, B);
+
         }
     }
 }
