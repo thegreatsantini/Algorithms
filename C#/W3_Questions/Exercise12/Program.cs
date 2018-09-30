743ce9e9180a25549107951452192a93aa6c6c72
thegreatsantini <lss13@my.fsu.edu>
2018-09-29 22:32:32 -0700

    solved more algos with c#

---
+++ C#/W3_Questions/Exercise12/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ C#/W3_Questions/Exercise8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise8
+{
+    class Program
+    {
+        public static void DisplayTable(int num) 
+        {
+            for (int i = 0; i <= 10; i++ ) {
+                Console.WriteLine("{0} * {1} = {2}", num, i, num * i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Select a number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            DisplayTable(num);
+        }
+    }
+}
