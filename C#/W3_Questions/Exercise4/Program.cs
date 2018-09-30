7960e9e13c93d741266b38be8a8f596407e4225f
thegreatsantini <lss13@my.fsu.edu>
2018-09-29 18:44:03 -0700

    completed an algo

--- C#/W3_Questions/Exercise4/Program.cs
+++ C#/W3_Questions/Exercise4/Program.cs
@@ -10,7 +10,6 @@ namespace Exercise4
             Console.WriteLine((35 + 5) % 7);
             Console.WriteLine(14 + -4 * 6 / 11);
             Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
-
         }
     }
 }
