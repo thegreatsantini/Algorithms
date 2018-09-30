c5bee742ba2ceb71b6637e962a51aa0c94cd81bd
thegreatsantini <lss13@my.fsu.edu>
2018-09-29 18:44:21 -0700

    completed an algo

--- C#/W3_Questions/Exercise6/Program.cs
+++ C#/W3_Questions/Exercise6/Program.cs
@@ -4,9 +4,12 @@ namespace Exercise6
 {
     class Program
     {
+        public static int MultiplyNums(int num1, int num2, int num3) {
+            return num1 * num2 * num3;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(MultiplyNums(2,3,6));
         }
     }
 }
