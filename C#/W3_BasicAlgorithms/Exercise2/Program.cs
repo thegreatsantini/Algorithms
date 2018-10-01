032258405a595be2d9a7e96e71fa921a7ebc6ba0
thegreatsantini <lss13@my.fsu.edu>
2018-09-30 18:03:30 -0700

    solved more algos with c#

---
+++ C#/W3_BasicAlgorithms/Exercise2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        private static int SubtractNums(int x)
+        {
+            return (x < 51) ?  51-x : Math.Abs(x - 51) * 3;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SubtractNums(12));
+            Console.WriteLine(SubtractNums(82));
+            Console.WriteLine(SubtractNums(53));
+        }
+    }
+}
---
+++ C#/W3_BasicAlgorithms/W3_BasicAlgorithms/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace W3_BasicAlgorithms
+{
+    class Program
+    {
+        private static int CheckNums(int num1, int num2)
+        {
+            return (num1 == num2) ? (num1 + num2) * 3 : num1 + num2;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CheckNums(1, 2));
+            Console.WriteLine(CheckNums(3, 3));
+            Console.WriteLine(CheckNums(4, 12));
+        }
+    }
+}
