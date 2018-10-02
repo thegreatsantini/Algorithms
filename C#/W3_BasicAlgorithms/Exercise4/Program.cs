6e73d407f051c4f6cf9fea0a5986266aac21d55d
thegreatsantini <lss13@my.fsu.edu>
2018-10-01 21:32:37 -0700

    solved more algos with c#

--- C#/W3_BasicAlgorithms/Exercise4/Program.cs
+++ C#/W3_BasicAlgorithms/Exercise4/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
-namespace Exercise4
+namespace Exercise5
 {
     class Program
     {
+        public static string CheckString(string myStr)
+        {
+            string[] sarr;
+            sarr = myStr.Split(' ');
+            if ( sarr[0] == "if" ) 
+                return myStr;
+            return "not else";
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(CheckString("if Hello World!"));
         }
     }
 }
