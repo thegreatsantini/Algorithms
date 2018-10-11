b7fa950ba09ab70b1caaaea3717259c5aa9f5991
thegreatsantini <lss13@my.fsu.edu>
2018-10-11 11:23:44 -0700

    went through some more Link Lists questions

--- Stack.js
+++ Stack.js
@@ -1,30 +1,35 @@
 //LIFO
-function MyStack ()  {
-    this.data = [];
+export default class MyStack {
+    constructor() {
+        this.data = [];
+    }
+    push(item) {
+        this.data.push(item);
+    }
+    remove() {
+        return this.data.pop();
+    }
+    peek() {
+        return this.data[0];
+    }
+    print() {
+        this.data.forEach(val => console.log(val));
+    }
+    getLength() {
+        return this.data.length;
+    }
 }
 
-MyStack.prototype.add = function (item) {
-    this.data.push(item);
-}
 
-MyStack.prototype.remove = function () {
-    return this.data.pop();
-}
 
-MyStack.prototype.print = function () {
-    this.data.forEach( val => console.log(val))
-}
 
-MyStack.prototype.getLength = function () {
-    return this.data.length;
-}
 const newStack = new MyStack();
 
-newStack.add(1)
-newStack.add(2)
-newStack.add(3)
-newStack.add(4)
-newStack.add(5)
+newStack.push(1)
+newStack.push(2)
+newStack.push(3)
+newStack.push(4)
+newStack.push(5)
 newStack.print()
 const size = newStack.getLength();
 console.log(size)
