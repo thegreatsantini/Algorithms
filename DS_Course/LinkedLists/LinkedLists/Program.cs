a794aee240e28c8d4a02d94cb0cb159f8e41fd3b
thegreatsantini <lss13@my.fsu.edu>
2018-10-12 21:06:34 -0700

    started linked list section for plurasight data sturctures course

--- DS_Course/LinkedLists/LinkedLists/Program.cs
+++ DS_Course/LinkedLists/LinkedLists/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
 
-namespace LinkedLists
+namespace NodeChains
 {
-    class Program
+    public class Node
     {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+    }
+    class Program 
+    {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //+-----+-----+
+            //|  3  | null+
+            //+-----|-----+
+            Node first = new Node { Value = 3 };
+
+            //+-----+-----+
+            //|  5  | null+
+            //+-----|-----+
+            Node middle = new Node { Value = 5 };
+
+            //+-----+-----+     +-----+-----+
+            //|  3  | null+---->+  5  + null+
+            //+-----|-----+     +-----+-----+
+            first.Next = middle;
+
+            //+-----+-----+
+            //|  7  | null+
+            //+-----|-----+
+            Node last = new Node { Value = 7 };
+
+
+            //+-----+-----+     +-----+-----+     +-----+------+
+            //|  3  | null+---->+  5  + null+---->+  7  + null +
+            //+-----|-----+     +-----+-----+     +-----+------+
+            middle.Next = last;
+
+            PrintList(first);
+        }
+        public static void PrintList(Node node) 
+        {
+            while ( node != null ) {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
         }
     }
 }
+
