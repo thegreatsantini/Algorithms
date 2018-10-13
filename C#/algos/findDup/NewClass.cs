8d5019f0fd213a6b9917d18202422946902abebd
thegreatsantini <lss13@my.fsu.edu>
2018-10-12 18:08:19 -0700

    some C# practice

---
+++ C#/algos/findDup/NewClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace findDup
+{
+    public class Course
+    {
+        int CourseID;
+        string CourseName;
+        public void CreateCourse(int ID, string name)
+        {
+            CourseID = ID;
+            CourseName = name;
+        }
+        public string CouseInfo()
+        {
+            return CourseName;
+        }
+    }
+}
---
+++ C#/algos/findDup/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace findDup
+{
+    class Solution
+    {
+        string TestString;
+        string[] SplitString;
+        List<string> duplicates = new List<string>();
+
+        public void SetString(string myString)
+        {
+            TestString = myString;
+        }
+        //public string[] SplitString()
+        //{
+        //    return TestString.Split(" ");
+        //}
+        public void PrintStringList()
+        {
+            Console.WriteLine(TestString);
+            foreach (char word in TestString)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //string myString = "the blue ball is blue";
+            //List<string> duplicates = new List<string>();
+
+            //string[] myList = myString.Split(" ");
+            var newSolution = new Solution();
+            newSolution.SetString("the blue ball, is blue!");
+            newSolution.SplitString();
+            newSolution.PrintStringList();
+
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+            //foreach (string value in myList)
+            //{
+            //    if ( !wordCount.ContainsKey(value) ) {
+            //        wordCount[value] = 1;
+            //    }
+            //    else {
+            //        wordCount[value]++;
+            //    }
+            //}
+
+            //foreach(KeyValuePair<string, int> item in wordCount ) 
+            //{
+            //    if ( item.Value > 1 ) {
+            //        duplicates.Add(item.Key);
+            //    }
+            //}
+
+            //foreach( string word in duplicates ) {
+            //    Console.WriteLine(word);
+            //}
+        }
+    }
+}
