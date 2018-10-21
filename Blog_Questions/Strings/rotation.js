00817dba9280251c9b7ed40ec897037451922f88
thegreatsantini <lss13@my.fsu.edu>
2018-10-20 21:17:42 -0700

    whiteboarding day with graeme

--- Blog_Questions/Strings/rotation.js
+++ Blog_Questions/Strings/rotation.js
@@ -2,20 +2,21 @@
 How do you check if two strings are a rotation of each other? 
 */
 
-const isRotation = (str1, str2) => {
+const isRotation = (str1) => {
     // create copy of string to modify original
-    let rotator = str1.slice();
-    
-    for (let char = 0; char < str1.length; char++) {
-        rotator = rotator.substring(1) + rotator.charAt(0);
-        if ( rotator === str2 ) {
-            return true;
-        }
+    // let rotator = str1.slice();
+
+    for (let char = 0; char < str1.length/2; char++) {
+        str1 = str1.substring(1) + str1.charAt(0);
+        console.log(str1)
+        // if ( str1 === str2 ) {
+        //     return true;
+        // }
     }
-    return false;
+    // return str1;
 }
 
 const myString = 'the cow jumped';
 const rotator = 'e cow jumpedth';
 
-console.log(isRotation(myString, rotator));
+console.log(isRotation(myString));
