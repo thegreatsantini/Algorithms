e80d4dbcfc1d3a6d38f980234de134ff4f20b630
thegreatsantini <lss13@my.fsu.edu>
2018-10-27 13:58:21 -0700

    complete a simple js fundementals practice

---
+++ JSprimitives.js
@@ -0,0 +1,43 @@
+const q1 = "" + 1 + 0; // 10
+// console.log(q1)
+
+const q2 = "" - 1 + 0; // -1
+// console.log(q2)
+
+const q3 = true + false; // 1
+// console.log(q3)
+
+const q4 = 6 / "3"; // 2
+// console.log(q4)
+
+const q5 = "2" * "3"; // 6
+// console.log(q5)
+
+const q6 = 4 + 5 + "px"; // "9px"
+// console.log(q6)
+
+const q7 = "$" + 4 + 5; // "$45"
+// console.log(q7)
+
+const q8 = "4" - 2; // 2
+// console.log(q8)
+
+const q9 = "4px" - 2; // "NaN"
+// console.log(q9)
+
+const q10 = 7 / 0; // Infinity
+// console.log(q10)
+
+const q11 = "  -9\n" + 5; // " "9 \n 5
+// console.log(q11)
+
+const q12 = "  -9\n" - 5; // -14 how????
+// console.log(q12)
+
+const q13 = null + 1; // 1
+// console.log(q13)
+
+const q14 = undefined + 1; // NaN
+console.log(q14)
+
+
