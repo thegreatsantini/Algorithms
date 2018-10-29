8ead4b73ba26e6fe7aa1ee90e2740ae29af77208
thegreatsantini <lss13@my.fsu.edu>
2018-10-28 22:21:31 -0700

    discussed js topics with graeme

--- playGround.js
+++ playGround.js
@@ -1,9 +1,26 @@
-var salary = "1000$";
+// var salary = "1000$";
 
- (function () {
-     console.log("Original salary was " + salary);
+//  (function () {
+//      console.log("Original salary was " + salary);
 
-     var salary = "5000$";
+//      var salary = "5000$";
 
-     console.log("My New Salary " + salary);
-})();
+//      console.log("My New Salary " + salary);
+// })();
+
+class Person {
+    constructor() {
+        this.name = '';
+        this.age = 0;
+    }
+
+    sayName() {
+        console.log("hello i am " + this.name)
+    }
+}
+
+
+const Lucas = new Person()
+Lucas.name = "Lucas"
+Lucas.sayName();
+console.log(Person)
