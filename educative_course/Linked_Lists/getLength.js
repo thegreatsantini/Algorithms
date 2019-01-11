060dc8a296f5f8bad38b9e58eff9abcebdc85d1a
thegreatsantini <lss13@my.fsu.edu>
2019-01-10 21:28:25 -0800

    get length linkedlist ; ;

---
+++ educative_course/Linked_Lists/getLength.js
@@ -0,0 +1,42 @@
+class Node {
+  constructor(val) {
+    (this.data = val), (this.next = null);
+  }
+}
+
+class LinkedList {
+  constructor(head) {
+    this.head = new Node(head);
+  }
+  insert(newVal) {
+    if (this.head === null) {
+      this.head = new Node(newVal);
+      return;
+    } else {
+      let currentNode = this.head;
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = new Node(newVal);
+      return;
+    }
+  }
+  getLength() {
+    let currentNode = this.head;
+    let length = 0;
+    while (currentNode) {
+      length++;
+      currentNode = currentNode.next;
+    }
+    return length;
+  }
+}
+
+const myList = new LinkedList(1);
+myList.insert(2);
+myList.insert(3);
+myList.insert(4);
+myList.insert(5);
+myList.insert(6);
+myList.insert(7);
+console.log(myList.getLength())
