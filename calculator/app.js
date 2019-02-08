7407d0e8545bb2483df66aadfa67fa5a892a73a5
thegreatsantini <lss13@my.fsu.edu>
2019-02-07 22:43:14 -0800

    good css ready for for js logic

--- calculator/app.js
+++ calculator/app.js
@@ -1,9 +1,11 @@
-const input = document.querySelector('#output');
-const btn = document.querySelectorAll('.btn');
-let display = '';
+const input = document.querySelector('.calculator__equation');
+const btn = document.querySelectorAll('.calculator__cell');
+let equation = '';
 let result = 0;
+input.innerHTML = 'hrell'
 btn.forEach(item => {
   item.addEventListener('click', (e)=> {
+    console.log(e.target.innerHTML)
     input.value += e.target.innerHTML
   })
 }  )
